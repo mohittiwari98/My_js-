f70bcb8367cad74a2f4019be71ad4ad69aef619f
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-10 12:58:36 +0530

    Create Number.js

---
+++ Basic/Number.js
@@ -0,0 +1,9 @@
+let firstFavNum = 20;
+let secondFavNum = 10;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
