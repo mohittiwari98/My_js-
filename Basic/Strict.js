62e483469b03ef25401ffaee91b5267bf02a49bc
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-10 13:02:13 +0530

    Create Strict.js

---
+++ Basic/Strict.js
@@ -0,0 +1,16 @@
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+
+// ***********************************
+// EQUALITY OPERATORS
+// === strict equality (Type + Value)
+// !== strict non-equality (Type + Value)
+// == Lose equality (values)
+// != Lose equality (values)
+
+console.log(10 === 10);
+console.log(10 !== 10);
+console.log(10 == 10);
+console.log(10 != 10);
