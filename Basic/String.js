0a10d56b354e7feb0e5ef8b39d8573c41ba5ddbd
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-10 13:08:58 +0530

    Add String manipulation functions in String.js

---
+++ Basic/String.js
@@ -0,0 +1,9 @@
+let f="mohit";
+let l="ravi";
+let full=f+" "+ l;
+let fullname=full.concat(full);
+console.log(full);
+console.log(fullname);
+console.log(full.length);
+console.log(full.toUpperCase());
+console.log(fullname.toLowerCase());
