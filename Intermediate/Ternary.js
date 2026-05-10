6301c891de27b0744893bb8fef22490620a99dac
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-10 22:49:30 +0530

    Add Ternary.js for password strength and voting eligibility

---
+++ Intermediate/Ternary.js
@@ -0,0 +1,9 @@
+let password=8;
+function pass(ps){
+    return ps===8?"strong pass":"weak pass"
+}
+console.log(pass(password));
+let age = 15;
+let result =
+  age >= 18 ? "You are eligible to vote." : "You are not eligible to vote yet";
+console.log(result);
