0bc69fa53513304cd1e31b658ffa0cd0e6f39427
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-10 17:56:35 +0530

    Create Filterexample.js with array filtering
    Added filtering examples for ages and words.

---
+++ Intermediate/Filterexample.js
@@ -0,0 +1,14 @@
+const ages = [32, 33, 16, 40];
+const a=ages.filter((ae)=>ae>18);
+console.log(a);
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
+
+const result = words.filter((word) => word.length > 6);
+console.log(result);
