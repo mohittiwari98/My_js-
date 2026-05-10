9a65daebb6ab1ec5fe2fcdd1f71edfbe5de6b067
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-10 23:13:11 +0530

    Add array methods to check people's name lengths

---
+++ Intermediate/E and s.js
@@ -0,0 +1,5 @@
+const peoples = ["huxn", "jordan", "alex"];
+const res=peoples.every((people)=>people.length>3);
+const res1=peoples.some((people)=>people.length>3);
+console.log(res);
+console.log(res1);
